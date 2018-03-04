d3cf5ec7666f9513c6f8cd82e1a4fe067ebc1ff9
jesusmoya95 <jesusmoyaguevara@hotmail.com>
2018-03-04 05:47:15 +0100

    Copia seguridad

---
+++ script_pa'lpueblo.js
@@ -0,0 +1,159 @@
+provincias = ['Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz',
+            'Cantabria','Castellón','Ciudad Real','Córdoba','La Coruña','Cuenca','Gerona','Granada','Guadalajara','Guipúzcoa','Huelva',
+            'Huesca','Islas Baleares','Jaén','León','Lérida','Lugo','Madrid','Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas',
+            'Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona','Santa Cruz de Tenerife','Teruel','Toledo','Valencia',
+            'Valladolid','Vizcaya','Zamora','Zaragoza']
+
+$(function(){
+
+    $( ".buscador" ).controlgroup();
+
+    // Buscar sitio
+    $( "#donde" ).autocomplete({
+        source: provincias
+    });
+
+    // Date llegada
+    $( "#llegada" ).datepicker({
+        minDate: 0,
+        showAnim: 'slideDown',
+        dateFormat: "dd/mm/yy"
+    });
+    
+    // Date salida
+    $( "#salida" ).datepicker({
+        showAnim: 'slideDown',
+        dateFormat: "dd/mm/yy",
+        minDate: 0
+    });
+    
+    // Tipo de vivienda
+    $( ".tipo input" ).checkboxradio({
+        icon: false
+    });
+
+    
+    // Llegada salida, control
+    var dateFormat = "dd/mm/yy",
+    from = $( "#llegada" )
+        .datepicker({
+            changeMonth: true,
+            dateFormat: "dd/mm/yy"
+        })
+        .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+        to.datepicker( "option", "dateFormat", "dd-mm-yy" );
+        from.datepicker( "option", "dateFormat", "dd-mm-yy" );
+        }),
+    to = $( "#salida" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3,
+        dateFormat: "dd/mm/yy"
+    })
+    .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+        
+    });
+
+    function getDate( element ) {
+    var date;
+    try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+        date = null;
+    }
+
+    return date;
+    }
+    // Fin control llegada - salida
+    
+    // Seleccion de personas
+    $( "#personas" )
+        .selectmenu()
+        .selectmenu( "menuWidget" )
+        .addClass( "overflow" );
+
+    // Precio slider
+    $( "#slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 20, 100 ],
+        step: 20,
+        slide: function( event, ui ) {
+            $( "#precio" ).val( ui.values[ 0 ] + "€ - " + ui.values[ 1 ] + "€" );
+        }
+    });
+    $( "#precio" ).val($( "#slider-range" ).slider( "values", 0 ) +
+    "€ - " + $( "#slider-range" ).slider( "values", 1 ) + "€" );
+    // Fin precio
+
+    // Características
+    $( ".caracteristicas input" ).checkboxradio({
+        icon: false,
+        classes: {"ui-checkboxradio": "highlight"}
+    });
+
+    // Ver más características
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        draggable: false,
+        dialogClass: "dialogo",
+        show: {
+          effect: "blind",
+          duration: 100
+        },
+        hide: {
+          effect: "explode",
+          duration: 100
+        },
+
+        //close: function(event, ui){
+        //    $( 'body' ).css('background-color', '');
+        //}
+    });
+   
+    $( "#opener" ).on( "click", function() {
+        if ($( "#dialog" ).dialog( "isOpen" ) == false){
+            $( "#dialog" ).dialog( "open" );
+        } else{
+            $( "#dialog" ).dialog( "close" )
+        }
+        //$( 'body' ).css('background-color', '#E5E5E5');
+    });
+
+    /* Abrir más características y que el body se ponga oscuro
+    if ($( '.dialogo' ).css('display') == "none"){
+        $( 'body' ).css('background-color', 'black');
+    }
+    $( '.dialogo' ).hover(function() {
+        $( 'body' ).css('background-color', 'black');
+        $('body').css('opacity', '.3');
+        $( '.dialogo' ).css('background-color', 'rgba(f, f, f, 1)');
+        $( '.dialogo' ).css('opacity', '1');
+        $( '.nada' ).prepend($( '.dialogo' ).css('display'));
+      }, function() {
+        // vuelve a dejar el <div> como estaba al hacer el "mouseout"
+        $('body').css('background-color', '');
+        $('body').css('opacity', '');
+      });*/
+
+    // Boton buscar
+    $( "#buscar" ).button({
+        icon: "ui-icon-search"
+    })
+
+    // Control de imagenes
+    $( "#previous" ).button({
+        icon: "ui-icon-arrowthick-1-w"
+    })
+    $( "#next" ).button({
+        icon: "ui-icon-arrowthick-1-e"
+    })
+
+    
+
+
+
+});
